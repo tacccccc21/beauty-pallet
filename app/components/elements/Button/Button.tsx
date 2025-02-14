d374218d687193b71e4cb634e60d4fcd2c69b85c
t.nakagawa <t.nakagawa@mips.co.jp>
2025-02-14 17:52:44 +0900

    文字追加

--- app/components/elements/Button/Button.tsx
+++ app/components/elements/Button/Button.tsx
@@ -1,19 +1,14 @@
 'use client';
 
-import React, { useState } from 'react';
+import Link from 'next/link';
 import './button.scss';
 
 const Button = () => {
-  const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount((count) => count + 1);
-  };
 
   return (
     <>
-      <button onClick={handleClick}>Button</button>
-      <div className="count">{count}</div>
+       <Link href="" className='anc'></Link>
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import "./index.scss";
 
 export default function Home() {
   return (
-    <main >
+    <main>
       <section className="mv">
           <img src="/top/test_mv.png" alt="" />
           <div className="title">
-            <p className="catchcopy">あなたの“好き”が、誰かのキレイに。</p>
+            <p className="catchcopy">あなたの“好き”が、<br/>誰かのキレイに。</p>
             <h1 className="siteName">Beauty Palette</h1>
           </div>
+      </section>
+
+      <section className="about">
+          
       </section>
     </main>
   );
